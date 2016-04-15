cca69262283308f6c35aebbe82cba5c24ec5e680
ldumorti <ldumorti@det-lauduml2.winad.priv>
2016-04-15 09:11:26 +0200

    copyright

--- lib.php
+++ lib.php
@@ -25,7 +25,7 @@
  * Moodle is performing actions across all modules.
  *
  * @package    mod_cobra
- * @copyright  2015 Laurence Dumortier UNamur
+ * @copyright  2016 - Cellule TICE - Unversite de Namur
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
