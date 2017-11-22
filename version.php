7b7125f7134184e94dd2b68fa715bc4dbefdf764
Jean-Roch Meurisse <jean-roch.meurisse@fundp.ac.be>
2017-11-22 15:00:22 +0100

    bump up version number to update lang var

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_cobra';
-$plugin->version = 2017082802;
+$plugin->version = 2017082803;
 $plugin->release = 'v2.0';
 $plugin->requires = 2016120503;
 $plugin->maturity = MATURITY_STABLE;
