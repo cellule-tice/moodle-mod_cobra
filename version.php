295e4d11babeafe7cef376040d5a7cb8fb90f134
jrm-unamur <jean-roch.meurisse@unamur.be>
2018-07-19 15:49:36 +0200

    bump up version number for release

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_cobra';
-$plugin->version = 2018071800;
-$plugin->release = 'v3.5';
+$plugin->version = 2018071900;
+$plugin->release = 'v3.5.1';
 $plugin->requires = 2018051700.00; // Moodle 3.5 (Build: 20180517).
 $plugin->maturity = MATURITY_RC; // Will be changed to MATURITY_STABLE on approval.
