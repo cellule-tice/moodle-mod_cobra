a0c8d08a08e554040148e418e7cafa4d07f4e0b7
jmeuriss <jean-roch.meurisse@unamur.be>
2018-11-19 10:15:39 +0100

    remove french lang file from github repo and bump up version number to update lang strings

--- version.php
+++ version.php
@@ -26,10 +26,8 @@
  */
 
 defined('MOODLE_INTERNAL') || die();
-
 $plugin->component = 'mod_cobra';
-$plugin->version = 2018111604;
-$plugin->release = 'v2.0';
-$plugin->requires = 2018051700;
-$plugin->maturity = MATURITY_STABLE;
-$plugin->dependencies = array();
+$plugin->version = 2018111605;
+$plugin->release = 'v3.5.1';
+$plugin->requires = 2018051700; // Moodle 3.5 (Build: 20180517).
+$plugin->maturity = MATURITY_RC; // Will be changed to MATURITY_STABLE on approval.
